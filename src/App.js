e619e7e26db335f00a16a8e0b00df233b9f53caa
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-06-02 01:26:45 -0500

    simplify routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
-import Router from './router/Router.tsx';
+import React from 'react'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom'
+import  Iniciar  from './pages/IniciarSesionPage'
+import  HomePage from './pages/Homepage'
+import Registro from './pages/RegistroPage'
 
 function App() {
   return (
-    <>
-      <Router/>
-    </>
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/iniciar" element={<Iniciar />} />
+        <Route path="/registro" element={<Registro />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
